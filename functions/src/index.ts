bdb46cc5afeffe6770beae4b7bc316022368bb10
tamakiii <tamakiii@users.noreply.github.com>
2019-06-30 20:54:21 +0900

    sample HTTP function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const hello = functions.https.onRequest((request, response) => {
+  if (request.body.name === undefined) {
+    response.status(200).send("Hello from Firebase!");
+  }
+  if (request.body.name === "") {
+    console.log(request.body);
+    response.status(400).end();
+  }
+
+  response.status(200).send(`Hello ${request.body.name}`);
+});
